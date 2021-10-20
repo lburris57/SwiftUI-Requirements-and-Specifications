2f3623dde6a5d05b5fb1dbd7899cd56313761f10
Larry Burris <lburris@mac.com>
2021-10-20 15:14:53 -0400

    Initial Commit

---
+++ SwiftUI Requirements and Specifications/SwiftUI_Requirements_and_SpecificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Requirements_and_SpecificationsApp.swift
+//  SwiftUI Requirements and Specifications
+//
+//  Created by Larry Burris on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Requirements_and_SpecificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
